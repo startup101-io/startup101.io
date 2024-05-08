f055c263256f1be12d49a99176b6182d4d7d51e7
LpmRaven <liamraven@gmail.com>
2024-05-08 18:55:21 +1000

    adds initial pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import App from './App.tsx';
-import RootLayout from './layouts/root-layout/index.tsx';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RootLayout, loader as rootLoader } from './layouts/root-layout.tsx';
+import { ErrorPage } from './pages/error-page.tsx';
+import { User } from './pages/user-page.tsx';
 import './index.scss';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    children: [
+      {
+        path: "/:userId",
+        element: <User />,
+      },
+    ],
   },
+  // {
+  //   path: "/:userId",
+  //   element: <User />,
+  // },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-    <App />
   </React.StrictMode>,
 )
